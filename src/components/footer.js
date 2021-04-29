2f83b72a306904508800aa7736d7338181f3830c
Suritam-Nandy <13suritsuritam@gmail.com>
2021-04-29 12:27:09 +0530

    first commit

---
+++ src/components/footer.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer className="items-center mt-28">
+      <p className="text-center">
+        <span className="text-gray-600">
+          Open source by{' '}
+          <a className="text-indigo-600" href="https://github.com/denniskigen">
+            Dennis Kigen
+          </a>
+          <span className="ml-4">&copy;2020 - now</span>
+        </span>
+      </p>
+    </footer>
+  );
+};
+
+export default Footer;
